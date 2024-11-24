7b7036e4244474ee46c92035dcb752503bfe061e
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-24 02:51:43 -0400

    uptade

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,22 +1,6 @@
 import express from "express";
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: "Gato fazendo yoga",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 3,
-        descricao: "Gato fazendo panqueca",
-        imagem: "https://placecats.com/millie/300/150"
-    }
-];
+import conectarAoBanco from './src/config/dbConfig.js';
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const app = express();
 app.use(express.json());
@@ -25,17 +9,24 @@ app.listen(3000, () => {
     console.log("servidor escutando...");
 });
 
-app.get("/posts", (req, res) => {
+async function getTodosPosts() {
+    const db = conexao.db("aluraInstabyte");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+};
+
+app.get("/posts", async (req, res) => {
+    const posts = await getTodosPosts();
     res.status(200).json(posts);
 });
 
-function buscarPostporID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-};
+// function buscarPostporID(id) {
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id);
+//     });
+// };
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostporID(req.params.id);
-    res.status(200).json(posts[index]);
-});
+// app.get("/posts/:id", (req, res) => {
+//     const index = buscarPostporID(req.params.id);
+//     res.status(200).json(posts[index]);
+// });
